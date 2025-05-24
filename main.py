8a8d81e182b281b6e084bd16e8c6c334fdb79750
Amrx9 <amr.khaledt1@gmail..com>
2025-05-24 07:26:46 +0300

    feat: add file_path argu

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import sorted_list_of_dicts
 
 def get_book_text(file_path):
@@ -5,7 +6,11 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
-letter_numbers, words_nums = sorted_list_of_dicts(get_book_text('books/frankenstein.txt'))
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+letter_numbers, words_nums = sorted_list_of_dicts(get_book_text(sys.argv[1]))
 print("============ BOOKBOT ============")
 print("Analyzing book found at books/frankenstein.txt...")
 print("----------- Word Count ----------")
